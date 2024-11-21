6070a20ccfcf7c9ec3e58af92c5d839a60ca1da5
vaultdweller-2287 <zane.apatoff@gmail.com>
2024-11-21 18:14:13 -0500

    Update RUNME.sh

--- Pub-Desktop-Environment/RUNME.sh
+++ Pub-Desktop-Environment/RUNME.sh
@@ -7,6 +7,12 @@ flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.f
 
 flatpak install flathub io.github.vikdevelop.SaveDesktop
 
+sudo apt install git
+
+sudo rm -rf ~/.config/xfce4/
+
+git clone https://github.com/vaultdweller-2287/pub-linux/tree/main/Pub-Desktop-Environment/xfce4 ~/.config/
+
 echo "===INSTALLED UTILITES==="
 echo "===RUNNING SAVEDESKTOP==="
 echo "===CHECK OUR GITHUB REPO FOR INSTRUCTIONS AT==="

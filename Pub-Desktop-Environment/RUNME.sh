a6fefbb6ce4feaf84003dc51e8e155f9a347f59b
vaultdweller-2287 <zane.apatoff@gmail.com>
2024-11-21 19:08:55 -0500

    Update RUNME.sh

--- Pub-Desktop-Environment/RUNME.sh
+++ Pub-Desktop-Environment/RUNME.sh
@@ -1,17 +1,31 @@
 #!/bin/bash
 echo "===PUB COMMON ENVIRONMENT INSTALLATION==="
 
-sudo apt install flatpak
+#Detects if script are not running as root... 
+if [ "$UID" != "0" ]; then
+   #$0 is the script itself (or the command used to call it)...
+   #$* parameters...
+   if whereis sudo &>/dev/null; then
+     echo "Please type the sudo password for the user $USER"
+     sudo $0 $*
+     exit
+   else
+     echo "Sudo not found. You will need to run this script as root."
+     exit
+   fi 
+fi
+
+apt install flatpak
 
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
 flatpak install flathub io.github.vikdevelop.SaveDesktop
 
-sudo apt install git
+apt install git
 
-sudo rm -rf ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-panel.xml
+rm -rf ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-panel.xml
 
-sudo mv ~/Downloads/xfce4-panel.xml ~/.config/xfce4/xfonf/xfce-perchannel-xml/
+mv ~/Downloads/xfce4-panel.xml ~/.config/xfce4/xfonf/xfce-perchannel-xml/
 
 echo "===INSTALLED UTILITES==="
 echo "===RUNNING SAVEDESKTOP==="

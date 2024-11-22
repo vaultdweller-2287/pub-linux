75aed3bd56d6ee0dc5df63decb564c2b43b2170f
vaultdweller-2287 <zane.apatoff@gmail.com>
2024-11-21 19:23:26 -0500

    Update RUNME.sh

--- Pub-Desktop-Environment/RUNME.sh
+++ Pub-Desktop-Environment/RUNME.sh
@@ -6,20 +6,6 @@ ls
 echo "Please type in which folder is your home folder (case sensitive)"
 read name
 
-#Detects if script are not running as root... 
-if [ "$UID" != "0" ]; then
-   #$0 is the script itself (or the command used to call it)...
-   #$* parameters...
-   if whereis sudo &>/dev/null; then
-     echo "Please type the sudo password for the user $USER"
-     sudo $0 $*
-     exit
-   else
-     echo "Sudo not found. You will need to run this script as root."
-     exit
-   fi 
-fi
-
 apt install flatpak
 
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo

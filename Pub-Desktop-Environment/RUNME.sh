1c3d63d2b349117ef3240de35cc7ac6986466e83
vaultdweller-2287 <zane.apatoff@gmail.com>
2024-11-23 12:01:25 -0500

    i am going mentally insane

--- Pub-Desktop-Environment/RUNME.sh
+++ Pub-Desktop-Environment/RUNME.sh
@@ -8,7 +8,10 @@ flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.f
 
 flatpak install flathub io.github.vikdevelop.SaveDesktop
 
-apt install xfce4-panel-profiles
+apt install python3-pip
+apt install libxapp-gtk3
+pip install psutil-6.1.0-cp36-abi3-manylinux_2_12_x86_64.manylinux2010_x86_64.manylinux_2_17_x86_64.manylinux2014_x86_64.whl --break-system-packages
+apt install xfce4-panel-profiles 
 
 xfce4-panel-profiles load /home/newuser/Downloads/PubLayout.tar.bz2
 
